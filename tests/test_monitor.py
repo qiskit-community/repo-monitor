d6103215a6e9aaae7df733a5909b7ec771cde79c
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2021-08-04 15:03:39 -0400

    Project: initial setup

---
+++ tests/test_monitor.py
@@ -0,0 +1,14 @@
+"""Tests for monitor class."""
+import unittest
+from monitor import Monitor
+
+
+class TestMonitor(unittest.TestCase):
+    """Monitor class tests."""
+
+    def setUp(self) -> None:
+        self.monitor = Monitor()
+
+    def test_class(self):
+        """Tests class."""
+        self.assertTrue(isinstance(self.monitor.get_open_issues("Qiskit", "qiskit-terra"), list))
